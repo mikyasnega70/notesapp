41bb66b792d645e49592fd3090e685f02c5d6d54
mikyasnega70 <mikenega70@gmail.com>
2026-02-11 16:53:18 +0300

    add auth router

--- noteapp/main.py
+++ noteapp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .models import Base
 from .database import engine
-from .router import note
+from .router import note, auth
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ async def test():
     return {'status':'healthy'}
 
 app.include_router(note.router) 
+app.include_router(auth.router)
